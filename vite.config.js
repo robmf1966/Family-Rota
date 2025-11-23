9759738184a01415053bab87e8d1921194857540
robmf1966 <56730581+robmf1966@users.noreply.github.com>
2025-11-23 09:29:09 +0000

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,7 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-  import { defineConfig } from 'vite';
-    import react from '@vitejs/plugin-react';
-
-    export default defineConfig({
-      plugins: [react()],
-      base: '/', // Essential for clean routing
-    });
-    
+export default defineConfig({
+  plugins: [react()],
+  base: './', // CRITICAL FIX: Tells Vite to use relative paths for assets
+});
